cbc1562af9ee9ea69a8cf05b9c090a722381875e
Md. Shakil <shakil.eub.cse@gmail.com>
2024-02-29 17:43:29 +0600

    About us add

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def courses():
     return render_template("courses.html")
 
+@app.route('/aboutus')
+def aboutus():
+    return render_template("aboutus.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
