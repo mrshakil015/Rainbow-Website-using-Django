0a08e894c869bf9a16e8ad561cb8aa3910473d32
Shakil <shakil.eub.cse@gmail.com>
2024-02-29 13:45:15 +0600

    Course Page add

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/courses')
+def courses():
+    return render_template("courses.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
