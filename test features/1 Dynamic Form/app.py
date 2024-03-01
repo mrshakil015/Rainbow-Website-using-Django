2cc288050d6e0c35fe3c66aad111d6db6e2c306c
Shakil <shakil.eub.cse@gmail.com>
2024-03-01 17:26:49 +0600

    Folder Name Change

--- test features/1 Dynamic Form/app.py
+++ test features/1 Dynamic Form/app.py
@@ -19,11 +19,12 @@ def entrydata():
     designation = request.form['designation']
     course = request.form['course']
     image_file = request.files['image']
+    image_name = image_file.filename
     print("Image: ",image_file.filename)
     if image_file:
-        image_file.save('static/' + image_file.filename)
+        image_file.save('static/' + image_name)
 
-    data = {'studentname': studentname, 'institute': institute, 'designation': designation, 'course': course, 'image_file': image_file}
+    data = {'studentname': studentname, 'institute': institute, 'designation': designation, 'course': course, 'image_name': image_name}
     print(data['studentname'])
     
     return data
