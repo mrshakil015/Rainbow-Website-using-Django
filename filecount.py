8becac206dc49c4f3cb53cacb497c17186d3346d
Md. Shakil <shakil.eub.cse@gmail.com>
2024-05-28 14:59:22 +0600

    previews info delete

--- filecount.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def list_files_in_folder(folder_path):
-    for root, _, files in os.walk(folder_path):
-        for file_name in files:
-            print(file_name)
-
-folder_path = 'static/images'
-list_files_in_folder(folder_path)
