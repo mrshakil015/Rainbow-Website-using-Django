1898763a168fddbb163a8542dcdaba9d5698feb6
Md. Shakil <shakil.eub.cse@gmail.com>
2024-06-06 23:20:22 +0600

    New modification deployment code added

--- Rainbow Django/RainbowProject/RainbowProject/settings.py
+++ Rainbow Django/RainbowProject/RainbowProject/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-l^xn#2qt#mp_b%1_o7abqsbvvfca&lf2+&f!c$$$z^0&o@lbt5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic', # This must be added before 'django.contrib.staticfiles'
     'django.contrib.staticfiles',
     'RainbowApp',
 ]
@@ -31,6 +32,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # This must be added here after SecurityMiddleware & SessionMiddleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
